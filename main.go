d28ee46b5928f60926c0ef410ba2c299f7157d96
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2023-08-04 09:04:22 -0500

    Add calculation for finding the number of points in error.

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
 )
 
 func main() {
@@ -21,20 +22,71 @@ func main() {
 	p.X.Scale = plot.LinearScale{}
 	p.Y.Label.Text = "Count of values with error"
 
-	line, scatter, _ := plotter.NewLinePoints(pts)
-	line.Color = plotutil.Color(1)
-	p.Add(line, scatter, plotter.NewGrid())
-
-	line2, scatter2, _ := plotter.NewLinePoints(pts2)
-	line2.Color = plotutil.Color(2)
-	scatter2.GlyphStyle.Shape = plotutil.Shape(2)
-	p.Add(line2, scatter2, plotter.NewGrid())
-
-	p.Legend.Add("Current", line, scatter)
-	p.Legend.Add("Proposed", line2, scatter2)
+	plotLineScatter(p, pts, 1)
+	plotLineScatter(p, pts2, 2)
 	p.Legend.Top = true
 
 	p.Save(120*vg.Centimeter, 20*vg.Centimeter, "MapToIndex_Error.svg")
+
+	pts = boundError(0, MapToIndex)
+	pts2 = boundError(0, MapExperimental2)
+	fmt.Println(len(pts))
+	p = plot.New()
+	p.Title.Text = fmt.Sprintf("Error in MapToIndex, Scale %d", scale)
+	p.X.Label.Text = "Power of 2"
+	p.Y.Label.Text = "Count of values with error"
+
+	plotScatter(p, pts, 1)
+	plotScatter(p, pts2, 2)
+
+	p.Save(40*vg.Centimeter, 20*vg.Centimeter, "MapToIndex.svg")
+
+	scale = 4
+	maxIndex := 1024
+	p = plot.New()
+	p.Title.Text = fmt.Sprintf("Error in MapToIndex, Scale %d", scale)
+	p.X.Label.Text = "Index"
+	p.Y.Label.Text = "Error Count"
+	pts = FindBoundError(scale, maxIndex, MapToIndex)
+	plotLineScatter(p, pts, 1)
+	pts2 = FindBoundError(scale, maxIndex, MapExperimental2)
+	plotLineScatter(p, pts2, 2)
+	p.Legend.Top = true
+
+	p.Save(40*vg.Centimeter, 20*vg.Centimeter, "MapToIndex_Error_graph.svg")
+
+}
+
+func plotLineScatter(p *plot.Plot, pts plotter.XYs, kind int) {
+	line, scatter, _ := plotter.NewLinePoints(pts)
+	if kind == 1 {
+		line.Color = plotutil.Color(1)
+		scatter.GlyphStyle.Shape = draw.CrossGlyph{}
+		scatter.GlyphStyle.Color = plotutil.Color(1)
+		p.Legend.Add("Current", line, scatter)
+	}
+	if kind == 2 {
+		line.Color = plotutil.Color(2)
+		scatter.GlyphStyle.Shape = draw.PlusGlyph{}
+		scatter.GlyphStyle.Color = plotutil.Color(2)
+		p.Legend.Add("Proposed", line, scatter)
+	}
+	p.Add(line, scatter, plotter.NewGrid())
+}
+
+func plotScatter(p *plot.Plot, pts plotter.XYs, kind int) {
+	scatter, _ := plotter.NewScatter(pts)
+	if kind == 1 {
+		scatter.GlyphStyle.Shape = draw.CrossGlyph{}
+		scatter.GlyphStyle.Color = plotutil.Color(1)
+		p.Legend.Add("Current", scatter)
+	}
+	if kind == 2 {
+		scatter.GlyphStyle.Shape = draw.PlusGlyph{}
+		scatter.GlyphStyle.Color = plotutil.Color(2)
+		p.Legend.Add("Proposed", scatter)
+	}
+	p.Add(scatter, plotter.NewGrid())
 }
 
 // MapToIndex for any scale.
@@ -109,4 +161,87 @@ func lowerBound(index int, scale int) float64 {
 	// For example lowerBound(getBin(Math.SmallestNonzeroFloat64, 7), 7) == 0.0
 	// 2 ^ (index * 2 ^ (-scale))
 	return math.Exp2(math.Ldexp(float64(index), -scale))
+}
+
+func boundError(scale int, mapF func(float64, int) int) plotter.XYs {
+	pts := plotter.XYs{}
+	size := 100
+	start := 126
+	count := 1
+
+	x := 0
+	for i := start; i < start+count; i++ {
+		v := lowerBound(i, scale)
+		tmp := make([]int, size)
+		for j := size - 1; j >= 0; j-- {
+			v = math.Nextafter(v, 0)
+			tmp[j] = mapF(v, scale)
+		}
+
+		for j := 0; j < size; j++ {
+			pts = append(pts, plotter.XY{X: float64(x), Y: float64(tmp[j])})
+			x++
+		}
+
+		v = lowerBound(i, scale)
+		pts = append(pts, plotter.XY{X: float64(x), Y: float64(mapF(v, scale))})
+		x++
+
+		for j := 0; j < size; j++ {
+			v = math.Nextafter(v, math.Inf(1))
+			pts = append(pts, plotter.XY{X: float64(x), Y: float64(mapF(v, scale))})
+			x++
+		}
+	}
+
+	return pts
+}
+
+var max = 2000
+
+func FindBoundError(scale int, maxIdx int, mapF func(float64, int) int) plotter.XYs {
+	pts := make(plotter.XYs, maxIdx)
+	for i := 0; i < maxIdx; i++ {
+		y := boundTransitionDown(i, scale, mapF)
+		if y == -1 {
+			y = boundTransitionUp(i, scale, mapF)
+			if y == -1 {
+				y = 0
+			}
+			y = -y
+		}
+
+		pts[i] = plotter.XY{X: float64(i), Y: float64(y)}
+	}
+	return pts
+
+}
+
+func boundTransitionDown(startIdx int, scale int, mapF func(float64, int) int) int {
+	v := lowerBound(startIdx, scale)
+
+	count := -1
+	for mapF(v, scale) == startIdx {
+		count++
+		v = math.Nextafter(v, 0)
+		if count >= max {
+			return -1
+		}
+	}
+
+	return count
+}
+func boundTransitionUp(startIdx int, scale int, mapF func(float64, int) int) int {
+	v := lowerBound(startIdx, scale)
+
+	count := -1
+	for mapF(v, scale) != startIdx {
+		count++
+		v = math.Nextafter(v, math.Inf(1))
+		if count >= max {
+			return -1
+		}
+	}
+
+	return count
 }
